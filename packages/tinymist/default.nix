478be724d0a17dbd059627c475b688b064aa7f0a
parsifa1 <li.aldric@gmail.com>
2024-03-20 12:38:50 +0000

    fix:tinymist

--- packages/tinymist/default.nix
+++ packages/tinymist/default.nix
@@ -1,35 +1,44 @@
 {
   lib,
+  pkgs,
   rustPlatform,
   fetchFromGitHub,
-}:
-rustPlatform.buildRustPackage rec {
-  pname = "tinymist";
-  version = "v0.11.1-rc2";
+}: let
+  gitRev = "13e35624245b2c84d005eec2e0a2a70c30b84a3e"; # 示例 Git 版本哈希
+  gitDescribe = "v0.11.1-rc2"; # 示例 Git 描述，通常是最近的标签
+in
+  rustPlatform.buildRustPackage rec {
+    pname = "tinymist";
+    version = "v0.11.1-rc2";
 
-  src = fetchFromGitHub {
-    owner = "Myriad-Dreamin";
-    repo = pname;
-    rev = version;
-    hash = "sha256-iBisN3zpJy9sU2H/WVEH8i6q3xCl4kVpoGoNxIUEnt4=";
-  };
-
-  # cargoHash = "";
-
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      # # "typst-0.11.0" = "sha256-UzZ0tbC6Dhn178GQDyLl70WTp3h5WdaBCsEKgLisZ2M=";
-      # "reflexo-0.4.2-rc8" = "sha256-wegPTSiYQ7oxqLgQqP4SHGBHPlDpo3hZmQ+shlVALjo=";
-      "typst-0.11.0" = "sha256-UzZ0tbC6Dhn178GQDyLl70WTp3h5WdaBCsEKgLisZ2M=";
-      # "typst-assets-0.10.0" = "sha256-rfx9XLjAn+eDSGu99FxK19uohKge93PPN4VHQkSLZA8=";
+    src = fetchFromGitHub {
+      repo = pname;
+      rev = version;
+      owner = "Myriad-Dreamin";
+      hash = "sha256-iBisN3zpJy9sU2H/WVEH8i6q3xCl4kVpoGoNxIUEnt4=";
     };
-  };
 
-  meta = with lib; {
-    description = "Tinymist [ˈtaɪni mɪst] is an integrated language service for Typst [taɪpst]. You can also call it '微霭' [wēi ǎi] in Chinese.";
-    homepage = "https://github.com/Myriad-Dreamin/tinymist";
-    mainProgram = "tinymist";
-    maintainers = with maintainers; [parsifa1];
-  };
-}
+    # cargoHash = "";
+    nativeBuildInputs = [pkgs.git];
+
+    buildPhase = ''
+      export VERGEN_GIT_SHA=${gitRev}
+      export VERGEN_GIT_DESCRIBE=${gitDescribe}
+
+      # 你的其他构建命令
+    '';
+
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      outputHashes = {
+        "typst-0.11.0" = "sha256-UzZ0tbC6Dhn178GQDyLl70WTp3h5WdaBCsEKgLisZ2M=";
+      };
+    };
+
+    meta = with lib; {
+      description = "Tinymist [ˈtaɪni mɪst] is an integrated language service for Typst [taɪpst]. You can also call it '微霭' [wēi ǎi] in Chinese.";
+      homepage = "https://github.com/Myriad-Dreamin/tinymist";
+      mainProgram = "tinymist";
+      maintainers = with maintainers; [parsifa1];
+    };
+  }
