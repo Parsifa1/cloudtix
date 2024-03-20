2938cb6a4723b53bdacd9f6bac765df68e83b448
parsifa1 <li.aldric@gmail.com>
2024-03-20 09:54:47 +0000

    fix:tinymist

--- packages/tinymist/default.nix
+++ packages/tinymist/default.nix
@@ -3,22 +3,27 @@
   rustPlatform,
   fetchFromGitHub,
 }:
-rustPlatform.buildRustPackage {
-  pname = "Tinymist";
-  # version = "0.11.0";
+rustPlatform.buildRustPackage rec {
+  pname = "tinymist";
+  version = "v0.11.1-rc1";
 
   src = fetchFromGitHub {
     owner = "Myriad-Dreamin";
-    repo = "tinymist";
-    rev = "a3948df5da08a18968fe8ec7efcebb63741054d7";
+    repo = pname;
+    rev = version;
     hash = "sha256-NZm7bOK00IKfH+Mof1/cNdyRW4nsESBYXGfXG1RzgLs=";
   };
 
+  # cargoHash = "";
+
   cargoLock = {
     lockFile = ./Cargo.lock;
-    # outputHashes = {
-    #   "typst-0.11.0" = "sha256-33333333333333333333333333333333333333333333";
-    # };
+    outputHashes = {
+      # # "typst-0.11.0" = "sha256-UzZ0tbC6Dhn178GQDyLl70WTp3h5WdaBCsEKgLisZ2M=";
+      # "reflexo-0.4.2-rc8" = "sha256-wegPTSiYQ7oxqLgQqP4SHGBHPlDpo3hZmQ+shlVALjo=";
+      "typst-0.11.0" = "sha256-UzZ0tbC6Dhn178GQDyLl70WTp3h5WdaBCsEKgLisZ2M=";
+      # "typst-assets-0.10.0" = "sha256-rfx9XLjAn+eDSGu99FxK19uohKge93PPN4VHQkSLZA8=";
+    };
   };
 
   meta = with lib; {
