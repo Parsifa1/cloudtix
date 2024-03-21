82f176283215eebad8bc712c753e8e88294c9814
parsifa1 <li.aldric@gmail.com>
2024-03-21 13:05:59 +0000

    add:add fonts

--- default.nix
+++ default.nix
@@ -2,5 +2,6 @@
   win32yank = pkgs.callPackage ./packages/win32yank.nix {};
   tinymist = pkgs.callPackage ./packages/tinymist {};
   typstyle = pkgs.callPackage ./packages/typstyle {};
+  IosevkaCloudtide = pkgs.callPackage ./packages/IosevkaCloudtide.nix {};
   # delance = pkgs.callPackage ./packages/delance.nix  {};
 }
