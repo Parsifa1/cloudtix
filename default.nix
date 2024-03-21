6f6dce9b1cd4bbd4d2664a9f6d22026f85d28485
parsifa1 <li.aldric@gmail.com>
2024-03-21 04:23:11 +0000

    fix: typstyle

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 {pkgs}: {
   win32yank = pkgs.callPackage ./packages/win32yank.nix {};
   tinymist = pkgs.callPackage ./packages/tinymist {};
+  typstyle = pkgs.callPackage ./packages/typstyle {};
   # delance = pkgs.callPackage ./packages/delance.nix  {};
 }
