2774609e44ef21fff2b19cc2502f2ab53de1519e
parsifa1 <li.aldric@gmail.com>
2024-03-24 17:58:48 +0000

    fix: 字体

--- packages/IosevkaCloudtide.nix
+++ packages/IosevkaCloudtide.nix
@@ -6,7 +6,7 @@ stdenvNoCC.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/Parsifa1/IosevkaCloudtide/archive/refs/heads/${version}.tar.gz";
-    hash = "sha256-+OnS4ewCrnqIOH3zyo0TDdto7W+fsxmw7VuVRFs2XYo=";
+    hash = "sha256-+wrGkd80WC4DsN/8xywj4V76cl0LaaDW+ynLsq2kM8E=";
   };
 
   installPhase = ''
