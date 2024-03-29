8a98f9d83f180e3f11653afb8a86a185a9b1f0f9
Parsifa1 <li.aldric@gmail.com>
2024-03-29 07:52:52 +0000

    update: tinymist

--- packages/tinymist/default.nix
+++ packages/tinymist/default.nix
@@ -4,59 +4,50 @@
   fetchFromGitHub,
   pkg-config,
   libgit2,
-  git,
   openssl,
   zlib,
   stdenv,
   darwin,
-}: let
-  gitRev = "13e35624245b2c84d005eec2e0a2a70c30b84a3e"; # 示例 Git 版本哈希
-  gitDescribe = "v0.11.1-rc2"; # 示例 Git 描述，通常是最近的标签
-in
-  rustPlatform.buildRustPackage rec {
-    pname = "tinymist";
-    version = "0.11.1-rc2";
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "tinymist";
+  version = "0.11.1";
 
-    src = fetchFromGitHub {
-      owner = "Myriad-Dreamin";
-      repo = "tinymist";
-      rev = "v${version}";
-      hash = "sha256-iBisN3zpJy9sU2H/WVEH8i6q3xCl4kVpoGoNxIUEnt4=";
+  src = fetchFromGitHub {
+    owner = "Myriad-Dreamin";
+    repo = "tinymist";
+    rev = "v${version}";
+    hash = "sha256-fkUL6+lNPtNONf01vxeRSj8b6bz0pW+mNFIQrV0twKM=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "typst-0.11.0" = "sha256-UzZ0tbC6Dhn178GQDyLl70WTp3h5WdaBCsEKgLisZ2M=";
     };
+  };
 
-    env.VERGEN_GIT_SHA = gitRev;
-    env.VERGEN_GIT_DESCRIBE = gitDescribe;
-    doCheck = false;
+  nativeBuildInputs = [
+    pkg-config
+  ];
 
-    cargoLock = {
-      lockFile = ./Cargo.lock;
-      outputHashes = {
-        "typst-0.11.0" = "sha256-UzZ0tbC6Dhn178GQDyLl70WTp3h5WdaBCsEKgLisZ2M=";
-      };
-    };
-
-    nativeBuildInputs = [
-      pkg-config
-      git
+  buildInputs =
+    [
+      libgit2
+      openssl
+      zlib
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.CoreFoundation
+      darwin.apple_sdk.frameworks.CoreServices
+      darwin.apple_sdk.frameworks.Security
     ];
 
-    buildInputs =
-      [
-        libgit2
-        openssl
-        zlib
-      ]
-      ++ lib.optionals stdenv.isDarwin [
-        darwin.apple_sdk.frameworks.CoreFoundation
-        darwin.apple_sdk.frameworks.CoreServices
-        darwin.apple_sdk.frameworks.Security
-      ];
-
-    meta = with lib; {
-      description = "";
-      homepage = "https://github.com/Myriad-Dreamin/tinymist";
-      license = licenses.asl20;
-      maintainers = with maintainers; [];
-      mainProgram = "tinymist";
-    };
-  }
+  meta = with lib; {
+    description = "";
+    homepage = "https://github.com/Myriad-Dreamin/tinymist/";
+    license = licenses.asl20;
+    maintainers = with maintainers; [parsifa1];
+    mainProgram = "tinymist";
+  };
+}
