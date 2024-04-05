c19d07cc1afb81196e367aad92e52e1a6a4d220d
Parsifa1 <li.aldric@gmail.com>
2024-04-05 20:07:31 +0000

    fix:tinymist

--- default.nix
+++ default.nix
@@ -2,5 +2,6 @@
   win32yank = pkgs.callPackage ./packages/win32yank.nix {};
   typstyle = pkgs.callPackage ./packages/typstyle {};
   fonts = pkgs.callPackage ./packages/fonts.nix {};
+  tinymist = pkgs.callPackage ./packages/tinymist {};
   # delance = pkgs.callPackage ./packages/delance.nix  {};
 }
