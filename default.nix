0e783c5b7507dab09fd11adfcae2f8f1fad572bf
Parsifa1 <li.aldric@gmail.com>
2024-05-25 09:10:22 +0000

    clean unuse package

--- default.nix
+++ default.nix
@@ -1,8 +1,7 @@
 {pkgs}: {
   win32yank = pkgs.callPackage ./packages/win32yank.nix {};
-  typstyle = pkgs.callPackage ./packages/typstyle {};
+  typstyle = pkgs.callPackage ./packages/typstyle.nix {};
   fonts = pkgs.callPackage ./packages/fonts.nix {};
   tinymist = pkgs.callPackage ./packages/tinymist {};
   onnxruntime = pkgs.callPackage ./packages/onnxruntime.nix {};
-  # delance = pkgs.callPackage ./packages/delance.nix  {};
 }
