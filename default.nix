bc1b5fe37cb6a2794f994f69af2f74af2d388402
Parsifa1 <li.aldric@gmail.com>
2024-05-25 09:12:40 +0000

    delete: tinymist

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
 {pkgs}: {
   win32yank = pkgs.callPackage ./packages/win32yank.nix {};
   fonts = pkgs.callPackage ./packages/fonts.nix {};
-  tinymist = pkgs.callPackage ./packages/tinymist {};
   onnxruntime = pkgs.callPackage ./packages/onnxruntime.nix {};
 }
