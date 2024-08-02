9be4596bb6d33fdb329d864c1766b4047d1588b7
Parsifa1 <li.aldric@gmail.com>
2024-08-02 22:20:26 +0000

    fix: delete fastfetch

--- default.nix
+++ default.nix
@@ -2,5 +2,4 @@
   win32yank = pkgs.callPackage ./packages/win32yank.nix {};
   fonts = pkgs.callPackage ./packages/fonts.nix {};
   onnxruntime = pkgs.callPackage ./packages/onnxruntime.nix {};
-  fastfetch = pkgs.callPackage ./packages/fastfetch.nix {};
 }
