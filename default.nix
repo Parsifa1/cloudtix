125a0f7843b0e6cc380aaf598ee58e697ec784ef
Parsifa1 <li.aldric@gmail.com>
2025-01-05 10:42:55 +0000

    feat: add verilator v5.008

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
-{pkgs}: {
-  win32yank = pkgs.callPackage ./packages/win32yank.nix {};
-  fonts = pkgs.callPackage ./packages/fonts.nix {};
-  onnxruntime = pkgs.callPackage ./packages/onnxruntime.nix {};
+{ pkgs }:
+{
+  win32yank = pkgs.callPackage ./packages/win32yank.nix { };
+  fonts = pkgs.callPackage ./packages/fonts.nix { };
+  onnxruntime = pkgs.callPackage ./packages/onnxruntime.nix { };
+  verilator5008 = pkgs.callPackage ./packages/velilator.nix { };
 }
---
+++ packages/velilator.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  perl,
+  flex,
+  bison,
+  python3,
+  autoconf,
+  which,
+  help2man,
+  makeWrapper,
+  glibcLocales,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "verilator";
+  version = "5.008";
+
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-+eJBGvQOk5w+PyUF3aieuXZVeKNS4cKQqHnJibKwFnM=";
+  };
+
+  enableParallelBuilding = true;
+  buildInputs = [ perl ];
+  nativeBuildInputs = [
+    makeWrapper
+    flex
+    bison
+    python3
+    autoconf
+    help2man
+  ];
+  nativeCheckInputs = [ which ];
+
+  doCheck = stdenv.isLinux; # darwin tests are broken for now...
+  checkTarget = "test";
+
+  preConfigure = "autoconf";
+
+  postPatch = ''
+    patchShebangs bin/* src/{flexfix,vlcovgen} test_regress/{driver.pl,t/*.pl}
+  '';
+
+  postInstall = lib.optionalString stdenv.isLinux ''
+    for x in $(ls $out/bin/verilator*); do
+      wrapProgram "$x" --set LOCALE_ARCHIVE "${glibcLocales}/lib/locale/locale-archive"
+    done
+  '';
+
+  meta = with lib; {
+    description = "Fast and robust (System)Verilog simulator/compiler";
+    homepage = "https://www.veripool.org/wiki/verilator";
+    license = with licenses; [
+      lgpl3Only
+      artistic2
+    ];
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ thoughtpolice ];
+  };
+}
