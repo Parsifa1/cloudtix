ed8304a7e00cd4023cf8e8b6a5084cdce8fb71ab
Parsifa1 <li.aldric@gmail.com>
2025-08-20 18:18:37 +0800

    fix: remove old treesitter

--- default.nix
+++ default.nix
@@ -1,14 +1,18 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
+# packagesFromDirectoryRecursive {
+#   callPackage = pkgs.callPackage;
+#   directory = ./my-packages;
+# }
 {
-  win32yank = pkgs.callPackage ./packages/win32yank.nix { };
+
   fonts = pkgs.callPackage ./packages/fonts.nix { };
   awrit = pkgs.callPackage ./packages/awrit.nix { };
-  onnxruntime = pkgs.callPackage ./packages/onnxruntime.nix { };
-  verilator5008 = pkgs.callPackage ./packages/velilator.nix { };
   qsign = pkgs.callPackage ./packages/qsign.nix { };
-  tree-sitter = pkgs.callPackage ./packages/treesitter.nix { };
+  win32yank = pkgs.callPackage ./packages/win32yank.nix { };
   fastfetch = pkgs.callPackage ./packages/fastfetch.nix { };
   distrobox = pkgs.callPackage ./packages/distrobox.nix { };
+  onnxruntime = pkgs.callPackage ./packages/onnxruntime.nix { };
+  verilator5008 = pkgs.callPackage ./packages/velilator.nix { };
 }
